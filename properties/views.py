3516507dcc6eed2a5d4cd9d0e50956d18083b59e
Hadiiir <hadeer.gamal3010@gmail.com>
2025-07-12 01:36:38 +0300

    Add Property model with title, description, price, location fields

--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render
 from django.views.decorators.cache import cache_page
-from .models import Property  # Assuming you have a Property model
-
+from django.http import JsonResponse
+from .models import Property
 
 @cache_page(60 * 15)  # Cache for 15 minutes (60 seconds * 15)
 def property_list(request):
-    properties = Property.objects.all()
-    properties = get_all_properties()  # Use our cached queryset function
-
-    return render(request, 'properties/list.html', {'properties': properties})
+    properties = Property.objects.all().values('id', 'title', 'price', 'description')
+    return JsonResponse(list(properties), safe=False)
